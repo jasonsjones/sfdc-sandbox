fd01457be77f2d4605010227e34c948005f9462d
Jason <jasonjones@salesforce.com>
2017-02-22 20:53:51 -0800

    Add scripts and wire up skeleton src files

---
+++ server/src/server.js
@@ -0,0 +1,6 @@
+const debug = require('debug')('sfdc:server');
+
+(() => {
+    let port = 3000;
+    debug(`server started on port ${port}`);
+})();
---
+++ server/src/server.test.js
@@ -0,0 +1,9 @@
+const chai = require('chai');
+
+const expect = chai.expect;
+
+describe('Basic Testing Framework', () => {
+    it('is working', () => {
+        expect(true).to.be.true;
+    });
+});
