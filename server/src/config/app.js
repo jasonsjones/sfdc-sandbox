9e006188ddf377ac40fdfdcf35647858aa3c6ad2
Jason <jasonjones@salesforce.com>
2017-02-23 21:24:06 -0800

    Add basic app config

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -1,5 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
 
 export default app;
