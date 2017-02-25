040dbd531b23abf57bdf2a1fe991eb05348754d1
Jason <jasonjones@salesforce.com>
2017-02-24 16:25:43 -0800

    Remove server.test.js file

--- server/src/server.test.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-
-const expect = chai.expect;
-
-describe('Basic Testing Framework', () => {
-    it('is working', () => {
-        expect(true).to.be.true;
-    });
-});
