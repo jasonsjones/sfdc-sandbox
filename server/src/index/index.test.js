3a62d2ac940433f4006272d6adb14972e161a2c6
Jason <jasonjones@salesforce.com>
2017-02-25 18:22:22 -0800

    Add integration tests

--- server/src/index/index.test.js
+++ server/src/index/index.test.js
@@ -1,23 +1,49 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 
-//import app from '../config/app';
+import '../config/app';
 
 chai.use(chaiHttp);
-//const expect = chai.expect;
+const expect = chai.expect;
+const baseUrl = 'http://localhost:3000';
 
 describe('Index Route', () => {
 
     describe('GET /', () => {
 
-        it('returns json with status code 200');
+        it('returns json with status code 200', done => {
+            chai.request(baseUrl)
+                .get('/')
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(200);
+                    done();
+                });
+        });
 
-        it('returns json with message property that is a string');
+        it('returns json with message property that is a string', done => {
+            chai.request(baseUrl)
+                .get('/')
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(200);
+                    expect(res.body).to.have.property('message').that.is.a('string');
+                    done();
+                });
+        });
     });
 
     describe('GET /notFound', () => {
 
-        it('returns status code 404 when navigating to an unknown page');
+        it('returns status code 404 when navigating to an unknown page', done => {
+            chai.request(baseUrl)
+                .get('/notFound')
+                .end((err, res) => {
+                    expect(err).to.exist
+                    expect(res).to.have.status(404);
+                    done();
+                });
+        });
     });
 
 });
