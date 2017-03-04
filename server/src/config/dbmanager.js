fa3a127661b362f2334c7b53409b23a405e6d216
Jason <jasonjones@salesforce.com>
2017-03-03 21:38:07 -0800

    Add dbmanager for server

---
+++ server/src/config/dbmanager.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+export default function (config) {
+
+    let devConnection = mongoose.createConnection('mongodb://' + config.db.host + '/' + config.db.name);
+    initHandlers(devConnection);
+}
+
+function initHandlers(conn) {
+    conn.once('open', function () {
+        console.log('connected to mongo container...');
+    });
+    conn.on('error', console.error.bind(console, 'connection error'));
+    conn.on('disconnected', () => {
+        console.log(`Mongoose disconnected`);
+    });
+    initProcessHandlers(conn);
+}
+
+function initProcessHandlers(conn) {
+    process.removeAllListeners('SIGINT').removeAllListeners('SIGUSR2');
+    process.once('SIGUSR2', () => {
+        gracefulShutdown('nodemon restart', conn, () => {
+            process.kill(process.pid, 'SIGUSR2');
+        });
+    });
+
+    process.once('SIGINT', () => {
+        gracefulShutdown('app termination', conn, () => {
+            process.exit(0);
+        });
+    });
+}
+
+function gracefulShutdown(msg, conn, callback) {
+    conn.close(() => {
+        console.log(`Mongoose disconnected through ${msg}`);
+        callback();
+    })
+}
