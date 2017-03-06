fe468219ed7edbc57ba2c88d71f6edca05065908
Jason <jasonjones@salesforce.com>
2017-03-05 20:35:21 -0800

    Use global promise lib

--- server/src/config/dbmanager.js
+++ server/src/config/dbmanager.js
@@ -3,6 +3,8 @@ import config from './config';
 
 export default function () {
 
+    mongoose.Promise = global.Promise;
+
     let na44 = mongoose.createConnection('mongodb://' + config.db.host + '/' + config.db.dev.name);
     initHandlers(na44);
 
