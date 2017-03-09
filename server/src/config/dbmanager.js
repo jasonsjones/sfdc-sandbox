df2699bee0ac0d785203065a776fc91a1036a546
Jason <jasonjones@salesforce.com>
2017-03-08 20:57:57 -0800

    Add debug statements to the dbmanager

--- server/src/config/dbmanager.js
+++ server/src/config/dbmanager.js
@@ -1,3 +1,5 @@
+const debug = require('debug')('sfdc:dbmanager');
+
 import mongoose from 'mongoose';
 import config from './config';
 
@@ -6,28 +8,31 @@ export default function () {
     mongoose.Promise = global.Promise;
 
     let na44 = mongoose.createConnection('mongodb://' + config.db.host + '/' + config.db.dev.name);
-    initHandlers(na44);
-    initProcessHandlers(na44);
+    initHandlers(na44, 'na44');
 
     return {
         na44: na44
     }
 }
 
-function initHandlers(conn) {
+function initHandlers(conn, dbname) {
     conn.once('open', function () {
-        console.log('connected to mongo container...');
+        debug(`Connected to mongo container ${dbname}`);
     });
     conn.on('error', console.error.bind(console, 'connection error'));
     conn.on('disconnected', () => {
-        console.log(`Mongoose disconnected`);
+        debug(`Mongoose disconnected from ${dbname}`);
     });
     initProcessHandlers(conn);
 }
 
 function initProcessHandlers(conn) {
+    debug('*** removing all listeners from SIGINT and SIGUSR2')
     process.removeAllListeners('SIGINT').removeAllListeners('SIGUSR2');
-    process.once('SIGUSR2', () => {
+
+    debug('*** adding listeners from SIGINT and SIGUSR2');
+    process.on('SIGUSR2', () => {
+        console.log('*** in the SIGUSR2 callback; node restarting...');
         gracefulShutdown('nodemon restart', conn, () => {
             process.kill(process.pid, 'SIGUSR2');
         });
@@ -38,11 +43,12 @@ function initProcessHandlers(conn) {
             process.exit(0);
         });
     });
+    debug('*** adding listeners complete...');
 }
 
 function gracefulShutdown(msg, conn, callback) {
     conn.close(() => {
-        console.log(`Mongoose disconnected through ${msg}`);
+        debug(`Mongoose disconnected through ${msg}`);
         callback();
     })
 }
