86d699190830ee4332f6b81b9dbfb651fcfc944c
Jason <jasonjones@salesforce.com>
2017-03-15 21:08:13 -0700

    Add unit test for user controller

---
+++ server/src/users/user.controller.test.js
@@ -0,0 +1,43 @@
+import  chai from 'chai';
+import  sinon from 'sinon';
+import 'sinon-mongoose';
+import 'sinon-as-promised';
+
+import { User } from './user.model';
+import { UserController } from './user.controller';
+
+const expect = chai.expect;
+
+describe('User Controller', () => {
+    let req
+    let res
+
+    beforeEach(() => {
+        res = {
+            json: sinon.spy(),
+            status: sinon.spy()
+        };
+    });
+
+    describe('getUsers()', () => {
+       let UserMock;
+        beforeEach(() => {
+            UserMock = sinon.mock(User);
+            UserMock.expects('find').withArgs({})
+                .chain('exec')
+                .resolves([]);
+        });
+        afterEach(() => {
+            UserMock.restore();
+        });
+
+        it('calls res.json()', (done) => {
+            UserController.getUsers(req, res, function () {
+                UserMock.verify();
+                expect(res.json.calledOnce).to.be.true;
+                done();
+            });
+        });
+    });
+
+});
