a6b87c88bfd29e50aa67d5b74bd15a854a7d6d67
Jason <jasonjones@salesforce.com>
2017-03-31 09:35:45 -0700

    Add unit test for GET /api/user/:id

--- server/src/users/user.test.js
+++ server/src/users/user.test.js
@@ -7,6 +7,7 @@ chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('User Route', () => {
+    let aUserId;
 
     describe('GET /users', () => {
 
@@ -18,6 +19,7 @@ describe('User Route', () => {
                 .end((err, res) => {
                     expect(err).to.be.null;
                     response = res;
+                    aUserId = res.body.payload[3]._id,
                     done();
                 });
         });
@@ -33,5 +35,41 @@ describe('User Route', () => {
         it('returns json with payload property that is an array', () => {
             expect(response.body).to.have.property('payload').that.is.an('array');
         });
+    });
+
+    describe('GET /user/:id', () => {
+        let response;
+
+        before((done) => {
+            chai.request(app)
+                .get(`/api/user/${aUserId}`)
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    response = res;
+                    done();
+                });
+        });
+
+        it('returns json', () => {
+            expect(response.type).to.eql('application/json');
+        });
+
+        it('returns json with success property that is a boolean', () => {
+            expect(response.body).to.have.property('success').that.is.a('boolean');
+        });
+
+        it('returns json with payload property that is an object', () => {
+            expect(response.body).to.have.property('payload').that.is.an('object');
+        });
+
+        it('returns a payload that is the requested user', () => {
+            let user = response.body.payload;
+            expect(user).to.have.property('_id').that.is.an('string');
+            expect(user._id).to.equal(aUserId);
+            expect(user).to.have.property('email').that.is.an('string');
+            expect(user).to.have.property('admin').that.is.an('boolean');
+            expect(user.name).to.have.property('full').that.is.an('string');
+        });
+
     });
 });
