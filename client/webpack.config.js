00bcafe98caea76040ad5a0b9acb9c638d43d346
Jason <jasonjones@salesforce.com>
2017-04-09 18:07:23 -0700

    Add devServer obj to webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,5 +19,8 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './src/index.html'
         })
-    ]
+    ],
+    devServer: {
+        historyApiFallback: true
+    }
 }
