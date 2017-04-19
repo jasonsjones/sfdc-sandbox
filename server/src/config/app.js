795c2759862e0edda10805109e11ea19dfe89af8
Jason <jasonjones@salesforce.com>
2017-04-19 08:59:03 -0700

    Add cors to node server

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import initRoutes from './routes';
 
 const app = express();
+
+app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}));
