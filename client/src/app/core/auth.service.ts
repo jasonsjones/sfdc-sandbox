c6e1fc28b3ce9c85f23a14235f5510e31b9ff2ba
Jason <jasonjones@salesforce.com>
2017-04-24 10:10:15 -0700

    Redirect to /one on successful login

--- client/src/app/core/auth.service.ts
+++ client/src/app/core/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
@@ -11,7 +12,7 @@ export class AuthService {
     public token: string;
     public currentUser: IUser;
 
-    constructor(private http: Http) {}
+    constructor(private http: Http, private router: Router) {}
 
     isAuthenticated(): boolean {
         return !!this.token;
@@ -20,21 +21,25 @@ export class AuthService {
     login(username: string, password: string): Observable<boolean> {
         return this.http.post('http://localhost:3000/api/login', {username: username, password: password})
             .map((response: Response) => {
-                let token = response.json() && response.json().payload.token;
-                let user = response.json() && response.json().payload.user;
-                if (token) {
-                    this.token = token;
-                    this.currentUser = this.generateCurrentUser(user);
+                let success = response.json() && response.json().success;
+                if (success) {
+                    let token = response.json() && response.json().payload.token;
+                    let user = response.json() && response.json().payload.user;
+                    if (token) {
+                        this.token = token;
+                        this.currentUser = this.generateCurrentUser(user);
 
-                    // store user info and jwt in local storage to keep user
-                    // logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify({
-                        username: username,
-                        userId: user._id,
-                        token: token
-                    }));
+                        // store user info and jwt in local storage to keep user
+                        // logged in between page refreshes
+                        localStorage.setItem('currentUser', JSON.stringify({
+                            username: username,
+                            userId: user._id,
+                            token: token
+                        }));
+                        this.router.navigate(['/one']);
+                    }
                 }
-                return response.json().success;
+                return success;
             });
     }
 
@@ -43,6 +48,7 @@ export class AuthService {
         this.token = null;
         this.currentUser = null;
         localStorage.removeItem('currentUser');
+        this.router.navigate(['/login']);
     }
 
     private generateCurrentUser(user): IUser {
