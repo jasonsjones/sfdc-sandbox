3130da72413522af228a1e2e4a0638da33d770b7
Jason <jasonjones@salesforce.com>
2017-05-08 15:40:04 -0700

    Style some of the home tab

--- client/src/app/one/home.component.ts
+++ client/src/app/one/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: 'src/app/one/home.component.html'
+    templateUrl: 'src/app/one/home.component.html',
+    styleUrls: ['src/app/one/home.component.css']
 })
 export class HomeComponent { }
